573b6cf37f28750672049380df62a37463d4b48a
ZaidC <chouglemohdzaid@gmail.com>
2026-06-08 19:35:34 +0530

    Initial commit

---
+++ Conditionals_Statements/month.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+ cout<<"Enter month number "<<endl;
+ int number;
+ cin >> number ; 
+ 
+ if (number==1)
+{
+  cout<< "January"<<endl;
+}
+else if (number<1)
+{ 
+    cout<<"Invalid"<<endl;
+}
+else if (number==2)
+{ 
+    cout<<"Febuary"<<endl;
+}
+
+else if (number==3)
+{ 
+    cout<<"March"<<endl;
+}
+
+
+else if (number==4)
+{ 
+    cout<<"April"<<endl;
+}
+
+else if (number==5)
+{ 
+    cout<<"May"<<endl;
+}
+
+else if (number==6)
+{ 
+    cout<<"june"<<endl;
+}
+
+else if (number==7)
+{ 
+    cout<<"jully"<<endl;
+}
+
+else if (number==8)
+{ 
+    cout<<"August"<<endl;
+}
+
+else if (number==9)
+{ 
+    cout<<"September"<<endl;
+}
+
+else if (number==10)
+{ 
+    cout<<"October"<<endl;
+}
+
+else if (number==11)
+{ 
+    cout<<"November"<<endl;
+}
+
+
+else if (number==12)
+{ 
+    cout<<"December"<<endl;
+}
+
+
+    return 0;
+}
